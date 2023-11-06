938c2de71be4c899428f677eb0b8115214cc0501
CaraUhrich <cara@Caraciraptor>
2023-11-06 14:14:12 -0500

    code added

--- vault.js
+++ vault.js
@@ -1,9 +1,13 @@
 //psuedocode: create equations for each of the numbers in the combination and assigning them to variables. Create an alert to display the message and values of the variables.
 
 //first number should evaluate to 10 using operator -
+const num1 = 17 - 7;
 
 //second number should evaluate to 40 using operator *
+const num2 = 2 * 20;
 
 //third number should evaluate to 39 using operator +
+const num3 = 33 +6;
 
 //alert to create dialog box and template literal to display message and values
+alert(`You have received this message because you have been chosen to open an important vault. Here is the secret combination: ${num1}, ${num2}, ${num3}.`);
