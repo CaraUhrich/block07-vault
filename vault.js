c8c5911fae513b757be2d5d0a4cb6a4043dd32f3
CaraUhrich <cara@Caraciraptor>
2023-11-06 13:09:14 -0500

    files created and comments added

---
+++ vault.js
@@ -0,0 +1,9 @@
+//psuedocode: create equations for each of the numbers in the combination and assigning them to variables. Create an alert to display the message and values of the variables.
+
+//first number should evaluate to 10 using operator -
+
+//second number should evaluate to 40 using operator *
+
+//third number should evaluate to 39 using operator +
+
+//alert to create dialog box and template literal to display message and values
